1aee3ceff9f607fd040e4b013d30153674f33e90
machin3io <social@machin3.io>
2020-10-17 12:04:48 +0200

    bump version

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "MACHIN3tools",
     "author": "MACHIN3",
-    "version": (0, 4, 0),
+    "version": (0, 4, 1),
     "blender": (2, 83, 0),
     "location": "",
     "description": "Streamlining Blender 2.80 and beyond.",
