72a0dd6210bfef6c6720d76a28b6538358016837
machin3io <social@machin3.io>
2020-12-25 13:23:37 +0100

    CursorToOrigin() - support resetting only Location or only Rotation
    * update utils.scene.set_cursor() to support setting matrix
    * update and simplify CursorToSelected() to take the third argument into account

--- ui/operators/cursor.py
+++ ui/operators/cursor.py
@@ -1,5 +1,6 @@
 import bpy
 import bmesh
+from mathutils import Vector, Quaternion
 from ... utils.math import get_center_between_verts, average_locations, create_rotation_matrix_from_vertex, create_rotation_matrix_from_edge, create_rotation_matrix_from_face
 from ... utils.scene import set_cursor
 from ... utils.ui import popup_message
@@ -14,16 +15,21 @@ cursor = None
 class CursorToOrigin(bpy.types.Operator):
     bl_idname = "machin3.cursor_to_origin"
     bl_label = "MACHIN3: Cursor to Origin"
-    bl_description = "Reset Cursor location and rotation to world origin"
+    bl_description = "Reset Cursor Location and/or Rotation to World Origin\nALT: Only reset Cursor Location\nCTRL: Only reset Cursor Rotation"
     bl_options = {'REGISTER', 'UNDO'}
 
-    def execute(self, context):
+    def invoke(self, context, event):
+        if event.alt and event.ctrl:
+            popup_message("Hold down ATL, CTRL or neither, not both!", title="Invalid Modifier Keys")
+            return {'CANCELLED'}
 
         # ensure cursor is actuall shown
         if not context.space_data.overlay.show_cursor:
             context.space_data.overlay.show_cursor = True
 
-        set_cursor()
+        cmx = context.scene.cursor.matrix
+
+        set_cursor(location=cmx.to_translation() if event.ctrl else Vector(), rotation=cmx.to_quaternion() if event.alt else Quaternion())
 
         if get_prefs().cursor_set_transform_preset:
             global cursor
@@ -73,6 +79,7 @@ class CursorToSelected(bpy.types.Operator):
 
         active = context.active_object
         sel = [obj for obj in context.selected_objects if obj != active]
+        cmx = context.scene.cursor.matrix
 
         # make sure there is an active
         if sel and not active:
@@ -86,7 +93,7 @@ class CursorToSelected(bpy.types.Operator):
 
         # if in object mode with multiple selected ojects, pass it on to Blender's op
         if context.mode == 'OBJECT' and active and not sel:
-            self.cursor_to_active_object(active, only_location=event.alt, only_rotation=event.ctrl)
+            self.cursor_to_active_object(active, cmx, only_location=event.alt, only_rotation=event.ctrl)
 
             if set_transform_preset_and_draw_cursor_axes:
                 if get_prefs().activate_transform_pie and get_prefs().cursor_set_transform_preset:
@@ -98,7 +105,7 @@ class CursorToSelected(bpy.types.Operator):
             return {'FINISHED'}
 
         elif context.mode == 'EDIT_MESH':
-            self.cursor_to_editmesh(context, active, only_location=event.alt, only_rotation=event.ctrl)
+            self.cursor_to_editmesh(context, active, cmx, only_location=event.alt, only_rotation=event.ctrl)
 
             if set_transform_preset_and_draw_cursor_axes:
                 if get_prefs().activate_transform_pie and get_prefs().cursor_set_transform_preset:
@@ -138,7 +145,7 @@ class CursorToSelected(bpy.types.Operator):
 
         bpy.ops.machin3.set_transform_preset(pivot='CURSOR', orientation='CURSOR')
 
-    def cursor_to_editmesh(self, context, active, only_location, only_rotation):
+    def cursor_to_editmesh(self, context, active, cmx, only_location, only_rotation):
         bm = bmesh.from_edit_mesh(active.data)
         mx = active.matrix_world
 
@@ -148,24 +155,20 @@ class CursorToSelected(bpy.types.Operator):
             co = average_locations([v.co for v in verts])
 
             # create vertex world matrix components
-            if not only_rotation:
-                loc = mx @ co
+            loc = mx @ co
 
-            if not only_location:
-                v = bm.select_history[-1] if bm.select_history else verts[0]
-                rot = create_rotation_matrix_from_vertex(active, v)
+            v = bm.select_history[-1] if bm.select_history else verts[0]
+            rot = create_rotation_matrix_from_vertex(active, v)
 
         elif tuple(bpy.context.scene.tool_settings.mesh_select_mode) == (False, True, False):
             edges = [e for e in bm.edges if e.select]
             center = average_locations([get_center_between_verts(*e.verts) for e in edges])
 
             # create edge world matrix components
-            if not only_rotation:
-                loc = mx @ center
+            loc = mx @ center
 
-            if not only_location:
-                e = bm.select_history[-1] if bm.select_history else edges[0]
-                rot = create_rotation_matrix_from_edge(active, e)
+            e = bm.select_history[-1] if bm.select_history else edges[0]
+            rot = create_rotation_matrix_from_edge(active, e)
 
         elif tuple(bpy.context.scene.tool_settings.mesh_select_mode) == (False, False, True):
             faces = [f for f in bm.faces if f.select]
@@ -173,18 +176,16 @@ class CursorToSelected(bpy.types.Operator):
             center = average_locations([f.calc_center_median_weighted() for f in faces])
 
             # create face world matrix components
-            if not only_rotation:
-                loc = mx @ center
+            loc = mx @ center
 
-            if not only_location:
-                f = bm.faces.active if bm.faces.active and bm.faces.active in faces else faces[0]
-                rot = create_rotation_matrix_from_face(mx, f)
+            f = bm.faces.active if bm.faces.active and bm.faces.active in faces else faces[0]
+            rot = create_rotation_matrix_from_face(mx, f)
 
         # set the cursor location/rotation
-        set_cursor(None if only_rotation else loc, None if only_location else rot.to_quaternion())
+        set_cursor(location=cmx.to_translation() if only_rotation else loc, rotation=cmx.to_quaternion() if only_location else rot.to_quaternion())
 
-    def cursor_to_active_object(self, active, only_location, only_rotation):
+    def cursor_to_active_object(self, active, cmx, only_location, only_rotation):
         mx = active.matrix_world
         loc, rot, _ = mx.decompose()
 
-        set_cursor(None if only_rotation else loc, None if only_location else rot)
+        set_cursor(location=cmx.to_translation() if only_rotation else loc, rotation=cmx.to_quaternion() if only_location else rot)
--- utils/scene.py
+++ utils/scene.py
@@ -2,20 +2,23 @@ import bpy
 from mathutils import Vector, Quaternion
 
 
-def set_cursor(location=Vector(), rotation=Quaternion()):
-    """
-    set cursor location (Vector), and rotation (Quaternion)
-    note, that setting cursor.matrix has no effect unfortunately
-    """
+def set_cursor(matrix=None, location=Vector(), rotation=Quaternion()):
+    '''
+    NOTE: setting the cursor rotation will result in tiny float imprecision issues and so the cursor matrix will differ very slightly afterwards
+    '''
 
     cursor = bpy.context.scene.cursor
 
-    # set location
-    if location:
+    # setting cursor.matrix has no effect unless a selection event occurs, so set location and rotation individually
+    if matrix:
+        cursor.location = matrix.to_translation()
+        cursor.rotation_quaternion = matrix.to_quaternion()
+        cursor.rotation_mode = 'QUATERNION'
+
+    # use passed in location and rotation
+    else:
         cursor.location = location
 
-    # set rotation
-    if rotation:
         if cursor.rotation_mode == 'QUATERNION':
             cursor.rotation_quaternion = rotation
 
