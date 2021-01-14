a63517dc30891e68c3ddeaff3d6c6c4b61e8eca2
machin3io <social@machin3.io>
2021-01-14 18:40:06 +0100

    Threads() - support setting starting position via active face

--- operators/threads.py
+++ operators/threads.py
@@ -1,11 +1,10 @@
 import bpy
 from bpy.props import IntProperty, FloatProperty
 import bmesh
-from math import pi, cos, sin, sqrt
 from mathutils import Vector, Matrix
-from .. utils.draw import draw_points, draw_point, draw_vector
 from .. utils.selection import get_boundary_edges, get_edges_vert_sequences
 from .. utils.math import average_locations
+from .. utils.geometry import calculate_threads
 
 
 class Threads(bpy.types.Operator):
@@ -92,7 +91,7 @@ class Threads(bpy.types.Operator):
                     self.radius = (radius1 + radius2) / 2
 
                     # create point coordinates and face indices
-                    threads, bottom, top, height = generate_threads(segments=self.segments, loops=self.loops, radius=self.radius, depth=self.depth / 100, h1=self.h1, h2=self.h2, h3=self.h3, h4=self.h4, fade=self.fade / 100)
+                    threads, bottom, top, height = calculate_threads(segments=self.segments, loops=self.loops, radius=self.radius, depth=self.depth / 100, h1=self.h1, h2=self.h2, h3=self.h3, h4=self.h4, fade=self.fade / 100)
 
                     # build the faces from those coords and indices
                     verts, faces = self.build_faces(bm, threads, bottom, top, smooth=smooth)
@@ -110,9 +109,19 @@ class Threads(bpy.types.Operator):
                     selrot = Vector((0, 0, 1)).rotation_difference(selup)
                     bmesh.ops.rotate(bm, cent=center1, matrix=selrot.to_matrix(), verts=verts, space=Matrix())
 
-                    # then the first verts are aligned too
+                    # then the first verts are aligned too, get the first vert from the active face if its part of the selection
+                    if bm.faces.active and bm.faces.active in selfaces:
+                        active_loops = [loop for v in bm.faces.active.verts if v in verts1 for loop in v.link_loops if loop.face == bm.faces.active]
+
+                        if active_loops[0].link_loop_next.vert == active_loops[1].vert:
+                            v1 = active_loops[1].vert
+                        else:
+                            v1 = active_loops[0].vert
+                    else:
+                        v1 = verts1[0]
+
                     threadvec = verts[0].co - center1
-                    selvec = verts1[0].co - center1
+                    selvec = v1.co - center1
 
                     matchrot = threadvec.rotation_difference(selvec)
                     bmesh.ops.rotate(bm, cent=center1, matrix=matchrot.to_matrix(), verts=verts, space=Matrix())
@@ -123,6 +132,7 @@ class Threads(bpy.types.Operator):
                     # remove the initially selected faces
                     bmesh.ops.delete(bm, geom=selfaces, context='FACES')
 
+                    # recalculate the normals, usefull when doing inverted threads
                     bmesh.ops.recalc_face_normals(bm, faces=faces)
 
                     bmesh.update_edit_mesh(active.data)
@@ -187,114 +197,3 @@ class Threads(bpy.types.Operator):
                     f.edges[-1].smooth = False
 
         return [v for v in verts + bottom_verts + top_verts if v.is_valid], faces + bottom_faces + top_faces
-
-
-def generate_threads(segments=32, loops=4, radius=1, depth=0.1, h1=0.2, h2=0.0, h3=0.2, h4=0.0, fade=0.15):
-    '''
-    thread profile
-    # |   h4
-    #  \  h3
-    #  |  h2
-    #  /  h1
-    return coords and indices tuples for thread, bottom and top faces, as well as the total height of the thread
-    '''
-
-    height = h1 + h2 + h3 + h4
-
-    # fade determines how many of the segments falloff
-    falloff = segments * fade
-
-    # create profile coords, there are 3-5 coords, depending on the h2 and h4 "spacer values"
-    profile = [Vector((radius, 0, 0))]
-    profile.append(Vector((radius + depth, 0, h1)))
-
-    if h2 > 0:
-        profile.append(Vector((radius + depth, 0, h1 + h2)))
-
-    profile.append(Vector((radius, 0, h1 + h2 + h3)))
-
-    if h4 > 0:
-        profile.append(Vector((radius, 0, h1 + h2 + h3 + h4)))
-
-    # based on the profile create the thread coords and indices
-    pcount = len(profile)
-
-    coords = []
-    indices = []
-
-    bottom_coords = []
-    bottom_indices = []
-
-    top_coords = []
-    top_indices = []
-
-
-    for loop in range(loops):
-        for segment in range(segments + 1):
-            angle = segment * 2 * pi / segments
-
-            # create the thread coords
-            for pidx, co in enumerate(profile):
-
-                # the radius for individual points is always the x coord, except when adjusting the falloff for the first or last segments
-                if loop == 0 and segment <= falloff and pidx in ([1, 2] if h2 else [1]):
-                    r = radius + depth * segment / falloff
-                elif loop == loops - 1 and segments - segment <= falloff and pidx in ([1, 2] if h2 else [1]):
-                    r = radius + depth * (segments - segment) / falloff
-                else:
-                    r = co.x
-
-                # slightly increase each profile coords height per segment, and offset it per loop too
-                z = co.z + (segment / segments) * height + (height * loop)
-
-                # add thread coords
-                coords.append(Vector((r * cos(angle), r * sin(angle), z)))
-
-                # add bottom coords, to close off the thread faces into a full cylinder
-                if loop == 0 and pidx == 0:
-
-                    # the last segment, has coords for all the verts of the profile!
-                    if segment == segments:
-                        bottom_coords.extend([Vector((radius, 0, co.z)) for co in profile])
-
-                    # every other segment has a point at z == 0 and the first point in the profile
-                    else:
-                        bottom_coords.extend([Vector((r * cos(angle), r * sin(angle), 0)), Vector((r * cos(angle), r * sin(angle), z))])
-
-                elif loop == loops - 1 and pidx == len(profile) - 1:
-
-                    # the first segment, has coords for all the verts of the profile!
-                    if segment == 0:
-                        top_coords.extend([Vector((radius, 0, co.z + height + height * loop)) for co in profile])
-
-                    # every other segment has a point at max height and the last point in the profile
-                    else:
-                        # top_coords.extend([Vector((r * cos(angle), r * sin(angle), 2 * height + height * loop)), Vector((r * cos(angle), r * sin(angle), z))])
-                        top_coords.extend([Vector((r * cos(angle), r * sin(angle), z)), Vector((r * cos(angle), r * sin(angle), 2 * height + height * loop))])
-
-
-            # for each segment - starting with the second one - create the face indices
-            if segment > 0:
-
-                # create thread face indices, pcount - 1 rows of them
-                for p in range(pcount - 1):
-                    indices.append([len(coords) + i + p for i in [-pcount * 2, -pcount, -pcount + 1, -pcount * 2 + 1]])
-
-                # create bottom face indices
-                if loop == 0:
-                    if segment < segments:
-                        bottom_indices.append([len(bottom_coords) + i for i in [-4, -2, -1, -3]])
-
-                    # the last face will have 5-7 verts, depending on h2 and h4
-                    else:
-                        bottom_indices.append([len(bottom_coords) + i for i in [-1 - pcount, -2 - pcount] + [i - pcount for i in range(pcount)]])
-
-                # create bottom face indices
-                if loop == loops - 1:
-                    # the first face will have 5-7 verts, depending on h2 and h4
-                    if segment == 1:
-                        top_indices.append([len(top_coords) + i for i in [-2, -1] + [-3 - i for i in range(pcount)]])
-                    else:
-                        top_indices.append([len(top_coords) + i for i in [-4, -2, -1, -3]])
-
-    return (coords, indices), (bottom_coords, bottom_indices), (top_coords, top_indices), height + height * loops
