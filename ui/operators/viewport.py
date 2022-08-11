926c1c2d5f968fd495193d3d953f16518476d81c
machin3io <social@machin3.io>
2022-08-11 09:08:00 +0200

    ViewAxis() - make alinging to avtive, local or cursor work again in 3.2
    * the problem is this is this Blender change: https://developer.blender.org/T100329
    * setting context.space_data.region_3d.is_orthographic_side_view = True will actually change the viewport to a side view now, thereby loosing the previous alignment to active/local/cursor
    * however setting this prop is necesassary for PreferencesInput.use_auto_perspective to work
    * there is no solution to this now, even manually settinp the view_matrix after is_orthographic_side_view doesn't work
    
    * so the best way to return to the perspective viewport afterwards, is to first run a normal side alignment, like top for instance
    * otherwise you could also just manually switch the view to perspective, but then you'll often also have to manually roll it for proper horizontal alignment

--- ui/operators/viewport.py
+++ ui/operators/viewport.py
@@ -1,8 +1,6 @@
 import bpy
 import bmesh
-from bpy.props import EnumProperty, BoolProperty, StringProperty
-from mathutils import Matrix, Euler, Quaternion
-from math import radians
+from bpy.props import EnumProperty, BoolProperty
 from ... utils.view import reset_viewport
 from ... utils.math import average_locations
 from ... items import view_axis_items
@@ -48,49 +46,6 @@ class ViewAxis(bpy.types.Operator):
             # always use ortho for aligned views like this
             r3d.view_perspective = 'ORTHO'
 
-            # """
-            # setting these props is required for prefs.inputs.use_auto_perspective to work
-            r3d.is_orthographic_side_view = True
-            r3d.is_perspective = True
-
-            # but no longer works as of 3.2, or even 3.1
-            # TODO: report it
-
-            # """
-
-            """
-            print("\nregion")
-            for r in dir(context.region):
-                print(r, getattr(context.region, r))
-
-            print("\nregion data")
-            for r in dir(context.region_data):
-                print(r, getattr(context.region_data, r))
-            """
-
-
-            # bl_rna
-            # clip_planes
-            # is_orthographic_side_view
-            # is_perspective
-            # lock_rotation
-            # perspective_matrix
-            # rna_type
-            # show_sync_view
-            # update
-            # use_box_clip
-            # use_clip_planes
-            # view_camera_offset
-            # view_camera_zoom
-            # view_distance
-            # view_location
-            # view_matrix
-            # view_perspective
-            # view_rotation
-            # window_matrix
-
-
-
         # align custom view in object or cursor space
         elif m3.custom_views_local or m3.custom_views_cursor:
             mx = context.scene.cursor.matrix if m3.custom_views_cursor else context.active_object.matrix_world if m3.custom_views_local and context.active_object else None
@@ -117,11 +72,6 @@ class ViewAxis(bpy.types.Operator):
             r3d.view_rotation = rot
 
             r3d.view_perspective = 'ORTHO'
-
-            # setting these props is required for prefs.inputs.use_auto_perspective to work
-            r3d.is_orthographic_side_view = True
-            r3d.is_perspective = True
-
 
         # align in world space
         else:
